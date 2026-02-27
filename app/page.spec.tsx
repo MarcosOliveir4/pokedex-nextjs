247300a6604314455aaa55b5ea9af58ed97ba673
MarcosOliveir4 <marcosassis071@gmail.com>
2026-02-27 10:42:37 -0300

    chore: adds CI configuration and .nvmrc file with Node.js version

---
+++ app/page.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Home from './page';
+
+describe('<Home />', () => {
+  it('should match snapshot', () => {
+    const { container } = render(<Home />);
+    expect(container).matchSnapshot();
+  });
+});
